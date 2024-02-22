c914c1c93f49972cc771bbca2eb3d82c48130322
TAeE <98029204+Haemyon@users.noreply.github.com>
2024-02-22 16:03:59 +0900

    Map1 Collision&Teleport to Start Point

---
+++ Source/SBSProject/SBSProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SBSProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SBSProject, "SBSProject" );
